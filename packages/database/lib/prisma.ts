ff037b1d4089a94075448b2e5cd33022fa4465f1
Kshitij Todkar <kshitijtodkar4826@gmail.com>
2023-10-13 22:02:51 +0530

    connection to DB and created first route handler

---
+++ packages/database/lib/prisma.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV != "production") globalForPrisma.prisma;
+
+// This code ensures that we only have one instance of the Prisma Client in our application, by using the global object to create a global variable prisma.By doing this, we can reuse the same prisma instance across our entire application.

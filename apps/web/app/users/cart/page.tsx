23683928a6837857f6ac90c7d52852c187b7c9e9
Kshitij Todkar <kshitijtodkar4826@gmail.com>
2023-11-02 01:18:04 +0530

    made cart responsive

--- apps/web/app/users/cart/page.tsx
+++ apps/web/app/users/cart/page.tsx
@@ -4,9 +4,11 @@ import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 import { ProductType } from 'types';
 import { CartItemCart } from "ui";
+import { Blackbutton } from "ui/components/Blackbutton";
 
-const UserProfile = () => {
+const Cart = () => {
     const [products, setProducts] = useState<ProductType[]>([]) ;
+    const [total, setTotal] = useState<number>(0) ;
     const { data:session } = useSession();
     //@ts-ignore
     const userId = session?.user?.id ;
@@ -19,6 +21,7 @@ const UserProfile = () => {
                 const data = await response?.json() ;
                 console.log(data) ;
                 setProducts(data.cartItems);
+                calculateTotal();
             }
             if(userId)
             {
@@ -29,15 +32,35 @@ const UserProfile = () => {
         }
     } , [userId])
 
+    const calculateTotal = () => {
+        let totalCost = products.reduce((accumulator, product) => { return accumulator + parseFloat(product.price) } , 0) ;
+        totalCost = parseFloat(totalCost.toFixed(2)) ;
+        console.log(totalCost);
+        setTotal(totalCost) ;
+    }
+
     return(
         <section className="flex flex-col items-center">
             <h1 className="text-red-500 font-bold text-5xl text-center">
                 Cart
             </h1>
-            <div className="flex flex-col gap-4 w-full items-center">
-                { products.map((product) => <div className="w-[4/5] sm:w-[95%] lg:w-[75%]" key={product.id}> <CartItemCart {...product}/> </div>)}
+            <div className="flex flex-col gap-6 w-full items-center">
+                { products.map((product) => 
+                  <div key={product.id}>
+                    <CartItemCart {...product} setTotal={setTotal} abc={() => total}/> 
+                  </div>
+                  )
+                }
             </div>
+            {
+                products.length ? 
+                <>
+                   <div onClick={() => { calculateTotal() ; alert(total) }} className="m-4">
+                      <Blackbutton name="Checkout" />
+                   </div>
+                </> : <></>
+            }
         </section>
     )
 }
-export default UserProfile
+export default Cart
